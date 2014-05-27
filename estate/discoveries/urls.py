76c9ee2dfd23e65d0f7a9bf802f2e31f08052056
Daniel Lete <daniel.lete@gmail.com>
2014-05-27 03:13:32 -0700

    Add sample views aa and ab to discoveries to test POST verb in forms

--- estate/discoveries/urls.py
+++ estate/discoveries/urls.py
@@ -4,6 +4,8 @@ from discoveries import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
+    url(r'^aa/', views.aa, name='aa'),
+    url(r'^ab/', views.ab, name='ab'),
     url(r'^reports/', views.reports, name='reports'),
     url(r'^scan/', views.scan, name='scan'),
 )
--- estate/discoveries/views.py
+++ estate/discoveries/views.py
@@ -1,20 +1,51 @@
 from django.shortcuts import render
 
 # from now down, not defaults
-from django.http import HttpResponse
-from django.template import RequestContext, loader
 from discoveries.models import PartDiscovered
 
+def aa(request):
+    context = {'bodymessage': "AA page of the discoveries app"}
+    context['titlemessage'] = "AA"
+
+    parts_discovered = PartDiscovered.objects.all()
+    context['parts_discovered'] = parts_discovered
+    return render(request, 'discoveries/aa.html', context)
+
+
+def ab(request):
+    context = {'bodymessage': "AB page of the discoveries app"}
+    context['titlemessage'] = "AB"
+
+    if request.POST['choice']:
+        c = request.POST['choice']
+        pieza = PartDiscovered.objects.get(pk=c)
+        context['pieza'] = pieza
+    else:
+        context['pieza'] = "vacio"
+
+    if request.POST['vehicle']:
+        v = request.POST.getlist('vehicle')
+        context['vehiculo'] = v
+    else:
+        context['vehiculo'] = "vacio"
+
+    return render(request, 'discoveries/ab.html', context)
+
+
 def index(request):
-    return HttpResponse("This is the INDEX page of the discoveries app")
+    context = {'boldmessage': "This is the INDEX page of the discoveries app. I am using templates"}
+    return render(request, 'discoveries/index.html', context)
 
 def reports(request):
     number_of_parts_discovered = PartDiscovered.objects.all().count()
-    template = loader.get_template('discoveries/reports.html')
-    context = RequestContext(request, {
-        'number_of_parts_discovered': number_of_parts_discovered,
-    })
-    return HttpResponse(template.render(context))
+    context = {'number_of_parts_discovered': number_of_parts_discovered}
+    context['boldmessage'] = "This is the REPORTS page of the discoveries app. I am using templates"
+    return render(request, 'discoveries/reports.html', context)
+
 
 def scan(request):
-    return HttpResponse("This is the SCAN page of the discoveries app")
+    context = {'boldmessage': "This is the SCAN page of the discoveries app"}
+
+    number_of_parts_discovered = PartDiscovered.objects.all().count()
+    context['number_of_parts_discovered'] = number_of_parts_discovered
+    return render(request, 'discoveries/scan.html', context)
